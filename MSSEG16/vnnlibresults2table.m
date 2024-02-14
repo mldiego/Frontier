25a1136ae96879d775e8d85c5fac3be675ed8ef7
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2024-02-13 18:53:51 -0600

    Better table

--- MSSEG16/vnnlibresults2table.m
+++ MSSEG16/vnnlibresults2table.m
@@ -4,25 +4,25 @@ resultsnnv = dir('results/*.txt');
 
 % begin table
 fid = fopen("tableSummary.tex", "w");
-fprintf(fid, "\\begin{center}\n");
-fprintf(fid, "\\begin{tabular}{ c c c c}\n");
-fprintf(fid, "\\textbf{Property} & \\textbf{Result} & \\textbf{Verification Time} & \\textbf{Counterexample Search Time} \\");
+fprintf(fid, "\\scriptsize\n");
+fprintf(fid, "\\begin{longtable}{| l | c | c | c |}\n");
+fprintf(fid, "\\toprule");
+fprintf(fid, "\\textbf{Property} & \\textbf{Result} & \\textbf{V. Time} & \\textbf{C.S. Time} \\");
 fprintf(fid, "\\");
 fprintf(fid, "\n");
+fprintf(fid, "\\midrule\n");
 % process all results
 for i=1:height(resultsnnv)
     name = resultsnnv(i).name;
     name = split(name, '_');
     name = strjoin(name, '\\_');
     [res, vt, ft] = processResultFile("results/"+resultsnnv(i).name);
-    fprintf(fid, " $%s$ & %s & %f & %f \\", name, res, vt, ft);
+    fprintf(fid, "$%s$ & %s & %f & %f \\", name, res, vt, ft);
     fprintf(fid, "\\");
     fprintf(fid, "\n");
 end
- % cell1 & cell2 & cell3 \\ 
- % cell4 & cell5 & cell6 \\  
- % cell7 & cell8 & cell9    
-fprintf(fid,"\\end{tabular}\n");
+fprintf(fid, "\\bottomrule\n"); 
+fprintf(fid,"\\end{longtable}\n");
 fprintf(fid,"\\end{center}");
 fclose(fid);
 
